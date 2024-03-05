32229293033ddfc64da859fdf4fb59522f8f2be7
Abid Ali <info786aqua@gmail.com>
2024-03-05 22:35:02 +0500

    first commit

---
+++ Assignment 6/app.js
@@ -0,0 +1,12 @@
+// 7.Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+//  Be sure to enclose your operations in print statements to see the results.
+// 8.You should create four lines that look like this:
+// console.log(5 + 3)
+// Print Addition
+console.log(5 + 3);
+// Print Subtraction
+console.log(11 - 3);
+// Print Multiplication
+console.log(4 * 2);
+// Print Division
+console.log(16 / 2);
