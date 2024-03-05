c728aa21b0ff85260d4e9b7c390b975a8a8adcb0
Abid Ali <info786aqua@gmail.com>
2024-03-05 16:44:01 +0500

    first commit

---
+++ Assignment 1/app.js
@@ -0,0 +1,2 @@
+var personName = "Abid";
+console.log("Hello ".concat(personName, ", Would you like to learn some python today?"));
---
+++ Assignment 1/app.ts
@@ -0,0 +1,9 @@
+// Assignment # 1
+
+// Storing the person name in a variable
+
+let personName: string = "Abid";
+
+// Printing a message to the person 
+
+console.log(`Hello ${personName}, Would you like to learn some python today?`);
