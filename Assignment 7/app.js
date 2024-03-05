d30f765c5a343938564ef830e347945e77999856
Abid Ali <info786aqua@gmail.com>
2024-03-05 22:44:42 +0500

    first commit

---
+++ Assignment 7/app.js
@@ -0,0 +1,5 @@
+// 9.Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. 
+var favouriteNumber = 1;
+// Printing the message.
+console.log("My favorite number is ".concat(favouriteNumber));
---
+++ Assignment 7/app.ts
@@ -0,0 +1,10 @@
+// 9.Favorite Number: Store your favorite number in a variable. 
+// Then, using that variable, create a message that reveals your favorite number. 
+
+
+let favouriteNumber: Number = 1;
+
+
+// Printing the message.
+
+console.log(`My favorite number is ${favouriteNumber}`);
