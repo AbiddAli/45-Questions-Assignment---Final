95a794913e1f19ca82f229c238cfa9644510e190
Abid Ali <info786aqua@gmail.com>
2024-03-05 20:18:26 +0500

    first commit

---
+++ Assignment 3/app.ts
@@ -0,0 +1,20 @@
+// 4. Printing the quote and the name of its author. 
+
+let quote: string= "A person who never made a mistake never tried anything new.";
+let author: string= "Albert Einstein"
+console.log(`${author} once said, "${quote}"`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
