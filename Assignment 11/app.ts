fade6e92eac359c324140ff83a4773b7e5299e32
Abid Ali <info786aqua@gmail.com>
2024-03-06 01:09:21 +0500

    first commit

---
+++ Assignment 11/app.ts
@@ -0,0 +1,11 @@
+// 13.Your Own Array:Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+// Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+
+
+let transportation : string [] = ['Toyota Car','Honda Motorcycle','Sports Bicycle'];
+
+for(let i=0; i<transportation.length; i++){
+    
+console.log("I would like to own a " + transportation[i]);
+}
