b11f46ace919e7e1dfb9ae9816c6454c5dca7a05
Abid Ali <info786aqua@gmail.com>
2024-03-09 03:41:56 +0500

    first commit

---
+++ Assignment 16/main.ts
@@ -0,0 +1,38 @@
+
+// 18.Seeing the World: Think of at least five places in the world you’d like to visit.
+// Store the locations in a array. Make sure the array is not in alphabetical order.
+let placeToVisit: string[] = ["Turkey","China","Iran","Pakistan","Australia"];
+
+// Print your array in its original order.
+console.log(placeToVisit);
+
+// Print your array in alphabetical order without modifying the actual list.
+console.log(placeToVisit.sort());
+
+// Show that your array is still in its original order by printing it.
+console.log(placeToVisit);
+
+// Print your array in reverse alphabetical order without changing the order of the original list.
+console.log(placeToVisit.sort().reverse());
+
+// Show that your array is still in its original order by printing it again.
+console.log(placeToVisit);
+
+// Reverse the order of your list. Print the array to show that its order has changed.
+console.log(placeToVisit.reverse());
+
+// Reverse the order of your list again. Print the list to show it’s back to its original order.
+console.log(placeToVisit.reverse());
+
+// Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+let sortedArray = ["Turkey","China","Iran","Pakistan","Australia"];
+
+sortedArray.sort();
+console.log(sortedArray);
+
+// Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+sortedArray.reverse();
+console.log(sortedArray);
+
+
+
