ee0f97ce1df4b0ea76fbd135a40cb691ce9e92ac
Abid Ali <info786aqua@gmail.com>
2024-03-20 07:45:34 +0500

    first commit

---
+++ Assignment 19/main.js
@@ -0,0 +1,3 @@
+// 21.	They think of something you could store in a Typescript Object. Write a program that creates Objects containing these items.
+var person = { name: "Abid", fname: "Male", age: 30 };
+console.log(person);
---
+++ Assignment 19/main.ts
@@ -0,0 +1,6 @@
+// 21.	They think of something you could store in a Typescript Object. Write a program that creates Objects containing these items.
+
+let person: {name: string, fname: string , age: number} = {name:"Abid", fname: "Male" , age: 30}
+
+console.log(person);
+
